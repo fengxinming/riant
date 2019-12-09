d7dccf79408195c34b9b7946ae31badd958a9aaf
fengxinming <fxm0016@126.com>
2019-12-10 00:45:02 +0800

    chore(examples/react-app): 增加react-app事例

---
+++ examples/react-app/riant-scripts.js
@@ -0,0 +1,3 @@
+'use strict';
+
+module.exports = require('../../packages/riant-scripts/bin');
---
+++ examples/react-app/riant.config.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const { join } = require('path');
+
+if (process.env.NODE_ENV === 'development') {
+  process.stdout.isTTY = false;
+}
+
+// riant
+module.exports = {
+  alias: {
+    '@': join(__dirname, 'src')
+  },
+
+  chainWebpack(chainedConfig, env) {
+    // 剔除 console.log
+    chainedConfig.optimization
+      .minimizer('TerserPlugin')
+      .init((plugin)=> {
+        plugin.options.terserOptions.compress.pure_funcs = ['console.log'];
+        return plugin;
+      });
+  },
+
+  paths(paths, env) {
+    return paths;
+  }
+};
